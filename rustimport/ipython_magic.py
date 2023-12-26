0f7ef4194de51b7373cd2a867d27a294ec688ea9
Thomas J. Fan <thomasjpfan@gmail.com>
2023-12-26 18:03:07 -0500

    Use rustimport_jupyter

--- rustimport/ipython_magic.py
+++ rustimport/ipython_magic.py
@@ -63,7 +63,7 @@ class RustImportIPython(Magics):
     def rustimport(self, line: str, cell: str):
         args = magic_arguments.parse_argstring(self.rustimport, line)
 
-        lib_path = Path(get_ipython_cache_dir()) / "rustimport"
+        lib_path = Path(get_ipython_cache_dir()) / "rustimport_jupyter"
         lib_path.mkdir(exist_ok=True)
 
         key = [
@@ -80,7 +80,8 @@ class RustImportIPython(Magics):
             key.append(time.time())
 
         module_name = (
-            "_rustimport_magic_" + hashlib.sha1(str(key).encode("utf-8")).hexdigest()
+            "_rustimport_jupyter_magic_"
+            + hashlib.sha1(str(key).encode("utf-8")).hexdigest()
         )
 
         # PyO3 only allows modules to be loaded once. If module name is already in
