519b8ae826569741a2987b75b511a66b3656e33e
Thomas J. Fan <thomasjpfan@gmail.com>
2023-12-28 16:34:36 -0500

    Reduce diff

--- rustimport/__init__.py
+++ rustimport/__init__.py
@@ -41,9 +41,7 @@ from rustimport.error_handling import BuildError
 _logger = _logging.getLogger("rustimport")
 
 
-def imp(
-    fullname, opt_in: bool = False, force_rebuild: bool = settings.force_rebuild
-) -> ModuleType:
+def imp(fullname, opt_in: bool = False, force_rebuild: bool = settings.force_rebuild) -> ModuleType:
     """
     `imp` is the explicit alternative to using rustimport.import_hook.
 
@@ -63,7 +61,6 @@ def imp(
 
     if settings.release_mode:
         import importlib
-
         with dlopen_flags():
             return importlib.import_module(fullname)
 
@@ -78,12 +75,7 @@ def imp(
     return importable.load()
 
 
-def imp_from_path(
-    path,
-    fullname=None,
-    opt_in: bool = False,
-    force_rebuild: bool = settings.force_rebuild,
-) -> ModuleType:
+def imp_from_path(path, fullname=None, opt_in: bool = False, force_rebuild: bool = settings.force_rebuild) -> ModuleType:
     """
     `imp_from_path` serves the same purpose as `imp` except allows
     specifying the exact path of the rust file or crate.
@@ -102,7 +94,6 @@ def imp_from_path(
 
     if settings.release_mode:
         import importlib
-
         with dlopen_flags():
             return importlib.import_module(fullname)
 
@@ -116,12 +107,8 @@ def imp_from_path(
             return i.load()
 
 
-def build(
-    fullname,
-    opt_in: bool = False,
-    force_rebuild: bool = settings.force_rebuild,
-    release: bool = settings.compile_release_binaries,
-):
+def build(fullname, opt_in: bool = False, force_rebuild: bool = settings.force_rebuild,
+          release: bool = settings.compile_release_binaries):
     """
     `build` builds a extension module like `imp` but does not import the
     extension.
@@ -143,12 +130,8 @@ def build(
     return importable
 
 
-def build_filepath(
-    path,
-    opt_in: bool = False,
-    force_rebuild: bool = settings.force_rebuild,
-    release: bool = settings.compile_release_binaries,
-):
+def build_filepath(path, opt_in: bool = False, force_rebuild: bool = settings.force_rebuild,
+                   release: bool = settings.compile_release_binaries):
     """
     `build_filepath` builds a extension module like `build` but allows
     to directly specify a file path.
@@ -173,12 +156,8 @@ def build_filepath(
                 return importable
 
 
-def build_all(
-    root_directory,
-    opt_in: bool = True,
-    force_rebuild: bool = settings.force_rebuild,
-    release: bool = settings.compile_release_binaries,
-):
+def build_all(root_directory, opt_in: bool = True, force_rebuild: bool = settings.force_rebuild,
+              release: bool = settings.compile_release_binaries):
     """
     `build_all` builds a extension module like `build` for each eligible (that is,
     containing the "rustimport" header) source file within the given `root_directory`.
@@ -198,23 +177,19 @@ def build_all(
 
     _logger.info(f"Collecting rust extensions in {root_directory}…")
     for directory, subdirs, files in os.walk(root_directory, topdown=True):
-        if any(f.lower() == "cargo.toml" for f in files):
+        if any(f.lower() == 'cargo.toml' for f in files):
             if i := CrateImportable.try_create(directory, opt_in=opt_in):
                 importables.append(i)
             # We never recurse into subdirectories of crates:
             del subdirs[:]
         else:
             for file in files:
-                if os.path.splitext(file)[1] == ".rs":
-                    i = SingleFileImportable.try_create(
-                        os.path.join(directory, file), opt_in=opt_in
-                    )
+                if os.path.splitext(file)[1] == '.rs':
+                    i = SingleFileImportable.try_create(os.path.join(directory, file), opt_in=opt_in)
                     if i is not None:
                         importables.append(i)
 
-    _logger.info(
-        f"Found {len(importables)} {'extension' if len(importables) == 1 else 'extensions'}."
-    )
+    _logger.info(f"Found {len(importables)} {'extension' if len(importables) == 1 else 'extensions'}.")
     not_built = []
     for index, i in enumerate(importables):
         if should_rebuild(i, force_rebuild=force_rebuild, force_release=release):
@@ -224,10 +199,8 @@ def build_all(
             not_built.append(i)
 
     if not_built:
-        _logger.info(
-            f"Skipped building {len(not_built)} {'extension' if len(not_built) == 1 else 'extensions'} due"
-            f" to unchanged source files. Re-run with `--force-rebuild` to rebuild everything."
-        )
+        _logger.info(f"Skipped building {len(not_built)} {'extension' if len(not_built) == 1 else 'extensions'} due"
+                     f" to unchanged source files. Re-run with `--force-rebuild` to rebuild everything.")
     _logger.info("Completed successfully.")
 
 
@@ -244,12 +217,6 @@ def load_ipython_extension(ipython):
 
 
 __all__ = [
-    "settings",
-    "imp",
-    "imp_from_path",
-    "build",
-    "build_filepath",
-    "build_all",
-    "BuildError",
-    "load_ipython_extension",
+    'settings', 'imp', 'imp_from_path', 'build',
+    'build_filepath', 'build_all', 'BuildError',
 ]
